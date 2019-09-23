335c6ccfe646d2d79a739d93e8fe5ba193bf43fe
Parth Pancholi <codestack1611@gmail.com>
2019-09-24 01:16:10 +0530

    form data

--- modules/forms/src/main/java/com/liferay/forms/portlet/FormsPortlet.java
+++ modules/forms/src/main/java/com/liferay/forms/portlet/FormsPortlet.java
@@ -1,37 +1,37 @@
 package com.liferay.forms.portlet;
 
-import com.liferay.dynamic.data.mapping.model.DDMContent;
-import com.liferay.dynamic.data.mapping.model.DDMFormInstance;
-import com.liferay.dynamic.data.mapping.service.DDMContentLocalServiceUtil;
-import com.liferay.dynamic.data.mapping.service.DDMFormInstanceLocalServiceUtil;
-import com.liferay.dynamic.data.mapping.service.DDMFormInstanceRecordLocalService;
-import com.liferay.dynamic.data.mapping.service.DDMFormInstanceRecordLocalServiceUtil;
-import com.liferay.forms.constants.FormsPortletKeys;
-import com.liferay.portal.kernel.json.JSONArray;
-import com.liferay.portal.kernel.json.JSONException;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
-import com.liferay.portal.kernel.service.UserLocalServiceUtil;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.kernel.util.WebKeys;
-
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 import javax.portlet.Portlet;
 import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
 
 import org.osgi.service.component.annotations.Component;
+
+import com.liferay.dynamic.data.mapping.model.DDMFormInstance;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecord;
+import com.liferay.dynamic.data.mapping.service.DDMContentLocalServiceUtil;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceLocalServiceUtil;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceRecordLocalServiceUtil;
+import com.liferay.forms.constants.FormsPortletKeys;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
 
 /**
  * @author Parth
@@ -53,53 +53,57 @@ import org.osgi.service.component.annotations.Component;
 public class FormsPortlet extends MVCPortlet {
 	private final static Log _log = LogFactoryUtil.getLog(FormsPortlet.class.getClass());
 	
+	/* (non-Javadoc)
+	 * @see com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet#render(javax.portlet.RenderRequest, javax.portlet.RenderResponse)
+	 */
 	@Override
 	public void render(RenderRequest renderRequest, RenderResponse renderResponse) throws IOException, PortletException{
 		_log.info("render called.");
 		ThemeDisplay themeDisplay= (ThemeDisplay) renderRequest.getAttribute(WebKeys.THEME_DISPLAY);
-		List<String> dataList = new ArrayList<>();
-		List<String> userNameList = new ArrayList<>();
-		List<String> userIdList = new ArrayList<>();
 		long groupId = themeDisplay.getSiteGroupId();
 		
 		List<DDMFormInstance> formNameList = DDMFormInstanceLocalServiceUtil.getFormInstances(groupId);
+		List<DDMFormInstance> formNameListFinal = new ArrayList<>();
 		for (DDMFormInstance ddmFormInstance : formNameList) {
-			String formName = ddmFormInstance.getName(Locale.US);
-			_log.info("formName============="+formName);
+			if(ddmFormInstance.getUserId() == themeDisplay.getUserId())
+				formNameListFinal.add(ddmFormInstance);
 		}
-		
-		List<DDMContent> content = DDMContentLocalServiceUtil.getContents(groupId);
-		if(Validator.isNotNull(content) && !content.isEmpty()) {
-			_log.info("list size==========="+content.size());
-			String userName = null;
-			long userId = 0;
-			for (DDMContent ddmContent : content) {
-				userName = ddmContent.getUserName();
-				JSONArray jsonArray = null;
-				userNameList.add(userName);
-				userId = ddmContent.getUserId();
-				userIdList.add(String.valueOf(userId));
+		_log.info(formNameListFinal);
+		renderRequest.setAttribute("formNameList", formNameListFinal);
+		super.render(renderRequest, renderResponse);
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet#serveResource(javax.portlet.ResourceRequest, javax.portlet.ResourceResponse)
+	 */
+	@Override
+	public void serveResource(ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+			throws IOException, PortletException {
+		if(Validator.isNotNull(resourceRequest.getParameter("Id"))) {
+			List<Map<String, String>> dataList = new ArrayList<>();
+			Map<String, String> dataMap;
+			List<DDMFormInstanceRecord> fromInstances = DDMFormInstanceRecordLocalServiceUtil.getFormInstanceRecords(Long.parseLong(resourceRequest.getParameter("Id")));
+			for (DDMFormInstanceRecord ddmFormInstanceRecord : fromInstances ) {
 				try {
-					jsonArray = JSONFactoryUtil.createJSONObject(ddmContent.getData()).getJSONArray("fieldValues");
+					dataMap = new HashMap<String, String>();
+					String data = DDMContentLocalServiceUtil.getContent(ddmFormInstanceRecord.getStorageId()).getData();
+					_log.info(data);
+					JSONArray jsonArray = JSONFactoryUtil.createJSONObject(data).getJSONArray("fieldValues");
 					for (int i = 0; i < jsonArray.length(); i++) {
 						JSONObject dataJson = jsonArray.getJSONObject(i);
 						JSONObject value = JSONFactoryUtil.createJSONObject(dataJson.getString("value"));
 						String fieldValue = value.getString("en_US");
-						dataList.add(fieldValue);
+						dataMap.put("email", fieldValue);
+						dataList.add(dataMap);
 					}
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
+				} catch (PortalException e) {
 					e.printStackTrace();
 				}
-				
 			}
-			_log.info("dataList====="+dataList);
-			_log.info("userList====="+userNameList);
-			_log.info("userIdList====="+userIdList);
-			renderRequest.setAttribute("list", dataList);
-			renderRequest.setAttribute("userName", userNameList);
-			renderRequest.setAttribute("userIdList", userIdList);
-			super.render(renderRequest, renderResponse);
+			resourceRequest.setAttribute("data", dataList);
+			PrintWriter printout=resourceResponse.getWriter();
+		    printout.print(JSONFactoryUtil.createJSONArray(dataList).toJSONString());    
 		}
+		super.serveResource(resourceRequest, resourceResponse);
 	}
 }
